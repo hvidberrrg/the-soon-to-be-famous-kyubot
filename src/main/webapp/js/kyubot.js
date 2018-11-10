ac4cf5563e11c83b6e57dc5cae19eda6f3efee70
hvidberrrg <s.hvidberg@gmail.com>
2018-11-10 13:19:21 +0100

    Moved KyuBOT javascript to separate file

--- src/main/webapp/js/kyubot.js
+++ src/main/webapp/js/kyubot.js
@@ -1,3 +1,4 @@
+// Start the conversation as soon as the document is ready
 $(document).ready(function () {
     addTypingIndicator();
     $.post("/conversation",
@@ -14,46 +15,53 @@ $("#kyubotQuery").submit(function (e) {
     // serializes the form's elements before clearing the input
     var formData = form.serialize();
 
-    addMessage("user", $("#inputText").val());
-    // Clear input
-    $('#inputText').val([]);
-    addTypingIndicator();
+    // Add the user's message to the conversation
+    var inputText = $("#inputText");
+    var message = inputText.val();
+    if (message && (message = $.trim(message))) {
+        addMessage("user", message);
+        // Clear input
+        inputText.val([]);
+        // Indicate that the KyuBOT is preparing a reply
+        addTypingIndicator();
 
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: formData,
-        success: function (data) {
-            updateConversation(data);
-        }
-    });
+        // Get the KyuBOT's reply and update the conversation
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: formData,
+            success: function (data) {
+                updateConversation(data);
+            }
+        });
+    }
     e.preventDefault(); // avoid to execute the actual submit of the form.
 });
 
 function updateConversation(data) {
     // Store the Watson context
     $('#watson_context').val(JSON.stringify(data.context));
+    // Remove the animation indicating that the KyuBOT is "typing"
     removeTypingIndicator();
+    // Add the KyuBOT's reply to the conversation
     addMessage("kyubot", data.output.text[0])
-    //$('#result').html("-- " + data.output.text[0]);
 }
 
 function addMessage(type, message) {
-    $( ".messages" ).append("<div class='message'><div class='" + type + "'>" + message + "</div></div>");
+    $(".messages").append("<div class='message'><div class='" + type + "'>" + message + "</div></div>");
     scrollToBottom();
 }
 
 function addTypingIndicator() {
-    $( ".messages" ).append( "<div class='typing-indicator-container'><div class='typing-indicator'><span></span><span></span><span></span></div></div>");
+    $(".messages").append("<div class='typing-indicator-container'><div class='typing-indicator'><span></span><span></span><span></span></div></div>");
     scrollToBottom();
 }
 
 function scrollToBottom() {
-    scrollDiv = $(".messages");
-    scrollDiv.animate({ scrollTop: scrollDiv.prop("scrollHeight")}, 1000);
-    //$(".messages").animate({ scrollTop: $('.messages').prop("scrollHeight")}, 1000);
+    var scrollDiv = $(".messages");
+    scrollDiv.animate({scrollTop: scrollDiv.prop("scrollHeight")}, 1000);
 }
 
 function removeTypingIndicator() {
-    $( "div" ).remove( ".typing-indicator-container" );
+    $("div").remove(".typing-indicator-container");
 }
