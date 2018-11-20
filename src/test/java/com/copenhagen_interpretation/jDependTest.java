585cb90a5c0da4712a4f349b5b47bba50890e7a9
hvidberrrg <s.hvidberg@gmail.com>
2018-11-20 21:19:02 +0100

    Added package dependency constraints test

--- src/test/java/com/copenhagen_interpretation/jDependTest.java
+++ src/test/java/com/copenhagen_interpretation/jDependTest.java
@@ -1,39 +1,60 @@
 package com.copenhagen_interpretation;
 
+import jdepend.framework.DependencyConstraint;
 import jdepend.framework.JDepend;
 import jdepend.framework.JavaPackage;
+import jdepend.framework.PackageFilter;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 
 public class jDependTest {
     private static final String PACKAGE_PREFIX = "com.copenhagen_interpretation";
+    private static final int EXPECTED_NO_OF_PACKAGES = 7;
     private JDepend jDepend;
 
     @Before
     public void setUp() throws Exception {
-        jDepend = new JDepend();
+        PackageFilter filter = PackageFilter.all().including(PACKAGE_PREFIX).excludingRest();
+        jDepend = new JDepend(filter);
         String path = System.getProperty("user.dir") + "/target/the-soon-to-be-famous-kyubot/WEB-INF/classes";
         jDepend.addDirectory(path);
     }
 
     @Test
-    public void containsCyclesTest() {
+    public void testPackageDependencyConstraints() {
+        DependencyConstraint constraint = new DependencyConstraint();
+        JavaPackage guice = constraint.addPackage(PACKAGE_PREFIX + ".guice");
+        JavaPackage kyubot = constraint.addPackage(PACKAGE_PREFIX + ".kyubot");
+        JavaPackage kyubot_cron = constraint.addPackage(PACKAGE_PREFIX + ".kyubot.cron");
+        JavaPackage util = constraint.addPackage(PACKAGE_PREFIX + ".util");
+        JavaPackage watson = constraint.addPackage(PACKAGE_PREFIX + ".watson");
+        JavaPackage watson_client = constraint.addPackage(PACKAGE_PREFIX + ".watson.client");
+        JavaPackage watson_model = constraint.addPackage(PACKAGE_PREFIX + ".watson.model");
+
+        guice.dependsUpon(kyubot, kyubot_cron, util, watson, watson_client);
+        kyubot.dependsUpon(watson, watson_model);
+        kyubot_cron.dependsUpon(util, watson, watson_model);
+        watson_client.dependsUpon(util);
+        watson.dependsUpon(watson_client, watson_model);
+
+        jDepend.analyze();
+        assertTrue("Package dependency mismatch!", jDepend.dependencyMatch(constraint));
+    }
+
+    @Test
+    public void testPackageDependencyCycles() {
         int packagesChecked = 0;
-        int expectedPackages = 7;
         Collection<JavaPackage> packages = jDepend.analyze();
         for (JavaPackage javaPackage : packages) {
-            if (javaPackage.getName().startsWith(PACKAGE_PREFIX)) {
-                packagesChecked++;
-                assertFalse(javaPackage.getName() + " contains cycles ", javaPackage.containsCycle());
-            }
+            packagesChecked++;
+            assertFalse(javaPackage.getName() + " contains cycles ", javaPackage.containsCycle());
         }
-        assertEquals("Expected " + expectedPackages + " packages but actually checked " + packagesChecked, expectedPackages, packagesChecked);
+        assertEquals("Expected " + EXPECTED_NO_OF_PACKAGES + " packages but actually checked " + packagesChecked, EXPECTED_NO_OF_PACKAGES, packagesChecked);
         assertFalse(PACKAGE_PREFIX + " contains cycles.", jDepend.containsCycles());
     }
 }
